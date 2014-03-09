177cc8ebad20470b61bccbba1bd6ec409a739f92
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-03-08 19:26:38 -0500

    moving arm

---
+++ index.js
@@ -0,0 +1,61 @@
+var five = require("johnny-five"),
+board, myServo;
+board = new five.Board();
+board.on("ready", function() {
+  s1 = new five.Servo({
+    pin: 8,
+    range: [20, 160],
+    startAt: 90, // if you would like the servo to immediately move to a degree
+    center: false 
+  });
+
+  s2 = new five.Servo({
+    pin: 4,
+    range: [20, 160],
+    startAt: 90, // if you would like the servo to immediately move to a degree
+    center: false 
+  });
+
+  board.repl.inject({
+    s1: s1,
+    s2: s2
+  });
+
+
+  console.log('active')
+
+  var left = function(){
+    s1.min()
+    s2.min()
+    setTimeout(function(){
+      s1.center()
+      s2.center()
+    },500)
+  }
+  var right = function(){
+    s1.max()
+    s2.max()
+    setTimeout(function(){
+      s1.center()
+      s2.center()
+    },500)
+  }
+
+  board.repl.inject({
+    r: right,
+    l: left
+  });
+
+
+  function t(){
+    setTimeout(function(){
+      left()
+      t()
+    },3000)
+  }
+
+  t()
+});
+
+
+
